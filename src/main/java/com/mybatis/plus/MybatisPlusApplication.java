b9f74266df0db213995719c0a2fa378e84700155
houfan <houfan@htah.com.cn>
2021-10-29 10:23:43 +0800

    springboot project Initial commit

---
+++ src/main/java/com/mybatis/plus/MybatisPlusApplication.java
@@ -0,0 +1,18 @@
+package com.mybatis.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname MybatisPlusApplication
+ * @Description TODO
+ * @Date 2021/10/29 10:20
+ * @Created by 侯凡
+ */
+@SpringBootApplication
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+}
