f9eabfe5aaea731e7b7ca45e672d67c0371e60fe
houfan <houfan@htah.com.cn>
2021-10-29 10:31:26 +0800

    test mybatis-plus select

--- src/main/java/com/mybatis/plus/MybatisPlusApplication.java
+++ src/main/java/com/mybatis/plus/MybatisPlusApplication.java
@@ -1,5 +1,6 @@
 package com.mybatis.plus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Created by 侯凡
  */
 @SpringBootApplication
+@MapperScan("com.mybatis.plus.mapper")
 public class MybatisPlusApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/mybatis/plus/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.mybatis.plus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mybatis.plus.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
